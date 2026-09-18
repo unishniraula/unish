1da8fc1d385a6c18c923ed40a97b1d865eec3c18
UNISH NIRAULA <unishniraula.official@gmail.com>
2026-09-18 19:49:12 +0545

    Delete contact.js

--- contact.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const yearSpan = document.getElementById('year');
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-  }
-});
