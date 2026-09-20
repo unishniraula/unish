9a42010723a5d8f07c7388f05155fab673014186
UNISH NIRAULA <unishniraula.official@gmail.com>
2026-09-20 10:13:24 +0545

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Base global script functionality
-document.addEventListener('DOMContentLoaded', () => {
-  // Set current year in footer
-  const yearElem = document.getElementById('year');
-  if (yearElem) {
-    yearElem.textContent = new Date().getFullYear();
-  }
-});
